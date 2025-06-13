2022d29714ed7174b4407bfe61e6c8203c3d48b1
Ochis27 <ochispaulcatalin27@gmail.com>
2025-06-13 10:39:56 +0300

    added product page

---
+++ app/products/page.tsx
@@ -0,0 +1,16 @@
+import {mockProducts} from "@/lib/mocks";
+import ProductCart from "../ProductCart";
+
+export default function HomePage() {
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Home</h1>
+      <p>Showing {mockProducts.length} products</p>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {mockProducts.map((product) => (
+          <ProductCart key={product.id} product={product} />
+        ))}
+      </div>
+    </main>
+  );
+}
