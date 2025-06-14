35ee191770d63af959c3693a3a77b70f226a215f
Ochis27 <ochispaulcatalin27@gmail.com>
2025-06-14 21:05:14 +0300

    added loading state and skeleton loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>Hello World!</div>;
+import {prisma} from "@/lib/prisma";
+import ProductCard from "./ProductCard";
+
+type SearchParams = Promise<{[key: string]: string | string[] | undefined}>;
+
+export default async function HomePage() {
+  const products = await prisma.product.findMany();
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Home</h1>
+      <p>Showing {products.length} products</p>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </main>
+  );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,18 +1,3 @@
-import {prisma} from "@/lib/prisma";
-import ProductCart from "../ProductCart";
-
-export default async function HomePage() {
-  const products = await prisma.product.findMany();
-
-  return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Home</h1>
-      <p>Showing {products.length} products</p>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {products.map((product) => (
-          <ProductCart key={product.id} product={product} />
-        ))}
-      </div>
-    </main>
-  );
+export default function Products() {
+  return <h1 className="text-3xl font-bold mb-6">Products</h1>;
 }
